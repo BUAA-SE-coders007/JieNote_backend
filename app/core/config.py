5c78e5e461169735b744e6b0dffb4933baffc9eb
Fantasy lee <129943055+Fantasylee21@users.noreply.github.com>
2025-04-12 11:41:22 +0800

    [feat]: 实现登录注册的相关功能

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,12 @@
+import os
+from datetime import timedelta
+
 class Settings:
-    PROJECT_NAME: str = "JieNote Backend"
-    VERSION: str = "1.0.0"
+    PROJECT_NAME: str = "JieNote Backend" # 项目名称
+    VERSION: str = "1.0.0" # 项目版本
     SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:coders007@47.93.172.156:3306/JieNote"  # 替换为实际的用户名、密码和数据库名称
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")  # 替换为更安全的密钥
+    ALGORITHM: str = "HS256" # JWT算法
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30 # token过期时间
 
 settings = Settings()
