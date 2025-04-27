49a01c65528c19e2acea8ed27971987bc7393963
coders007 <22373362@buaa.edu.cn>
2025-04-27 11:20:58 +0800

    [fix]:fix redis config in redis.py

--- app/utils/redis.py
+++ app/utils/redis.py
@@ -1,5 +1,6 @@
 import redis
 import time
+import os
 
 redis_client = None  # 全局 Redis 客户端变量
 
@@ -13,7 +14,16 @@ def get_redis_client():
         while True:
             try:
                 print("Connecting to Redis...")
-                redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+                redis_client = redis.StrictRedis(
+                    # host=os.getenv("REDIS_HOST", "localhost"),  # 从环境变量读取 Redis 主机名
+                    # port=int(os.getenv("REDIS_PORT", 6379)),   # 从环境变量读取 Redis 端口
+                    # password=os.getenv("REDIS_PASSWORD", None),  # 从环境变量读取 Redis 密码
+                    host='redis',
+                    port=6379,
+                    password='OKredis',
+                    db=0,
+                    decode_responses=True
+                )
                 redis_client.ping()
                 print("Connected to Redis successfully.")
                 break
